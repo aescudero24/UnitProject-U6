e1b7030fb4fd7d0e13fa751938933e8ff787d5ab
Adrian Escudero <adrianescudero1305@gmail.com>
2024-01-28 23:46:55 -0600

    redone views and added new ones

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "app",
+    "app.apps.Config",
     "django_filters",
     'django.contrib.admin',
     'django.contrib.auth',
