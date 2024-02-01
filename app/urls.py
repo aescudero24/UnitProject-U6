86da0640b7bea7d166e4a1d015dd774ce3a94d4b
Adrian Escudero <adrianescudero1305@gmail.com>
2024-02-01 08:50:30 -0600

    added and edited new urls

--- app/urls.py
+++ app/urls.py
@@ -15,11 +15,11 @@ urlpatterns = [
     path('dashboard/', adminPage, name="dash"),
     
     path("pets/", petsPage, name="pets"),
+    path("adopt/<str:id>/", adoptionPage, name="adopt"),
     path("owner/<str:id>/", homePage, name="owner"),
 
     path("settings/", settingsPage, name="settings"),
 
     path("add-pet/", createPetPage, name="create"),
-    path("delete", deleteUser, name="deleteUser")
-
+    path("delete-user/<str:id>/", deleteUserPage, name="delete")
 ]
