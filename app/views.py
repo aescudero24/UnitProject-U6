9d0d38e57b46f4af95ac81aac59faab785dad68f
Adrian Escudero <adrianescudero1305@gmail.com>
2024-02-01 14:50:29 -0600

    added decorators

--- app/views.py
+++ app/views.py
@@ -20,7 +20,7 @@ from .decorators import *
 
 # Create your views here.
 		
-# @unauthenticated_user
+@unauthenticated_user
 def signupPage(request: HttpRequest) -> HttpResponse:
 	form = CreateUserForm()
 	if request.method == "POST":
@@ -33,7 +33,7 @@ def signupPage(request: HttpRequest) -> HttpResponse:
 	context = {"form":form}
 	return render(request, "signup.html", context)
 
-# @unauthenticated_user
+@unauthenticated_user
 def loginPage(request: HttpRequest) -> HttpResponse:
 	if request.method == "POST":
 		username = request.POST.get("username")
@@ -55,8 +55,8 @@ def homePage(request: HttpRequest) -> HttpResponse:
 	context = {}
 	return render(request, "home.html", context)
 
-# @login_required(login_url="login")
-# @admin_only
+@login_required(login_url="login")
+@admin_only
 def adminPage(request: HttpRequest) -> HttpResponse:
     adoptions = Adoption.objects.all()
     owners = Owner.objects.all()
@@ -74,8 +74,8 @@ def adminPage(request: HttpRequest) -> HttpResponse:
     return render(request, "admin.html", context)
 
 
-# @login_required(login_url="login")
-# @allowed_users(allowed_roles=["student"])
+@login_required(login_url="login")
+@allowed_users(allowed_roles=["owner"])
 def userPage(request: HttpRequest) -> HttpResponse:
 	adoptions = request.user.owner.adoption_set.all()
 	total_adoptions = adoptions.count()
@@ -85,8 +85,8 @@ def userPage(request: HttpRequest) -> HttpResponse:
 	"adopted":adopted,"available":available}
 	return render(request, "user.html", context)
 
-# @login_required(login_url="login")
-# @allowed_users(allowed_roles=["student"])
+@login_required(login_url="login")
+@allowed_users(allowed_roles=["owner"])
 def settingsPage(request: HttpRequest) -> HttpResponse:
 	owner = request.user.owner
 	form = OwnerForm(instance=owner)
@@ -97,19 +97,18 @@ def settingsPage(request: HttpRequest) -> HttpResponse:
 	context = {"form":form}
 	return render(request, "settings.html", context)
 
-# @login_required(login_url="login")
-# @allowed_users(allowed_roles=["admin"])
-from django.db.models import Subquery, OuterRef
-
+@login_required(login_url="login")
+@allowed_users(allowed_roles=["admin", "owner"])
 def petsPage(request):
     pets = Pet.objects.filter(status='Available')
     return render(request, 'pets.html', {'pets': pets})
 
 
+@login_required
+@allowed_users(allowed_roles=["owner"])
 def adoptionPage(request, id):
     pet = get_object_or_404(Pet, id=id)
     adoption_form = AdoptionForm()
-
     if request.method == 'POST':
         adoption_form = AdoptionForm(request.POST)
         if adoption_form.is_valid():
@@ -125,7 +124,8 @@ def adoptionPage(request, id):
             return redirect('pets')
     return render(request, 'adoption.html', {'pet': pet, 'adoption_form': adoption_form})
 
-#create pet page
+@login_required
+@admin_only
 def createPetPage(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = PetForm(request.POST, request.FILES)
@@ -137,11 +137,10 @@ def createPetPage(request: HttpRequest) -> HttpResponse:
 
     return render(request, 'create.html', {'form': form})
 
-#DeleteUser
-# @admin_only
+@login_required
+@admin_only
 def deleteUserPage(request, id):
     user_in_question = get_object_or_404(User, id=id)
-
     if request.user.is_superuser and request.user != user_in_question:
         if request.method == "POST":
             owner_to_delete = Owner.objects.get(user=user_in_question)
